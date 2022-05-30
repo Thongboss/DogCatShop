ad7599b756a604ec41be7bd91dade2aef4448056
thongvh <thongvh@tinhvan.com>
2022-05-30 17:51:02 +0700

    second-commit

---
+++ src/main/java/edu/poly/shop/domain/Product.java
@@ -0,0 +1,24 @@
+package edu.poly.shop.domain;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+	private int productId;
+	private String name;
+	private int quantity;
+	private double unitPrice;
+	private String image;
+	private String description;
+	private double discount;
+	private Date entereddate;
+	private short status;
+	private int categoryId;
+	
+}
